1eb2a3f5f7bd7bb8d92834c71ee5330a0b2c4f28
shahnawaz-ayon <62610777+shahnawaz-ayon@users.noreply.github.com>
2021-07-13 22:41:04 +0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,36 @@
+<script>
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+</script>
+
+<script type="text/javascript">
+(function(d, s) {
+    var useSSL = 'https:' == document.location.protocol;
+    var js, where = d.getElementsByTagName(s)[0],
+    js = d.createElement(s);
+    js.src = (useSSL ? 'https:' : 'http:') +  '//www.peopleperhour.com/hire/2888153996/5969018.js?width=300&height=135&orientation=vertical&theme=dark&rnd='+parseInt(Math.random()*10000, 10);
+    try { where.parentNode.insertBefore(js, where); } catch (e) { if (typeof console !== 'undefined' && console.log && e.stack) { console.log(e.stack); } }
+}(document, 'script'));
+</script>
+<script src="https://unpkg.com/aos@next/dist/aos.js"></script>		
+<script>
+  AOS.init({
+    offset:120,
+    duration: 1000,
+    once: false,
+
+  });
+
+</script>
